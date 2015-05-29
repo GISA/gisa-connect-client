ef5754cbb1cc26dc12122314e0c7f72cb295da07
Michael Wyraz <michael@wyraz.de>
2015-05-29 21:53:31 +0200

    Initial checkin

---
+++ src/main/java/de/gisa/connect/client/GisaConnectClient.java
@@ -0,0 +1,69 @@
+package de.gisa.connect.client;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.ShutdownSignalException;
+
+public class GisaConnectClient implements Closeable
+{
+    protected final Connection connection;
+
+    protected final Channel channel;
+
+    protected final String userPrefix;
+
+    protected final String exchangeName;
+
+    public GisaConnectClient(String host, String username, String password) throws IOException, TimeoutException
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(host);
+        factory.setVirtualHost("VH_ppu");
+        // FIXME: wrum geht hier nur "guest"?
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        connection = factory.newConnection();
+        channel = connection.createChannel(); // FIXME: 1 Channel für alles?
+                                              // FIXME: connection schließen,
+                                              // wenn Channel-create fehlschlägt
+
+        userPrefix = username + ".";
+        exchangeName = username + ".EXCHANGE";
+    }
+
+    public void publish(String routingTag, byte[] payload) throws IOException
+    {
+        channel.basicPublish(exchangeName, userPrefix + routingTag, null, payload);
+    }
+
+    public SimpleQueue consume(String queueName) throws IOException
+    {
+        SimpleQueue simpleQueue=new SimpleQueue();
+        channel.basicConsume(userPrefix+queueName, true, simpleQueue.consumer);
+        return simpleQueue;
+    }
+
+    public void close() throws IOException
+    {
+        try
+        {
+            if (channel.isOpen()) channel.close();
+        }
+        catch (TimeoutException ex)
+        {
+            ex.printStackTrace(); // FIXME: Logging
+        }
+        finally
+        {
+            if (connection.isOpen()) connection.close();
+        }
+    }
+}
---
+++ src/main/java/de/gisa/connect/client/SimpleQueue.java
@@ -0,0 +1,98 @@
+package de.gisa.connect.client;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.ShutdownSignalException;
+import com.rabbitmq.client.AMQP.BasicProperties;
+
+public class SimpleQueue
+{
+    public static class Message
+    {
+        protected String routingKey;
+        protected byte[] payload;
+        
+        public Message(String routingKey, byte[] payload)
+        {
+            super();
+            this.routingKey = routingKey;
+            this.payload = payload;
+        }
+        public String getRoutingKey()
+        {
+            return routingKey;
+        }
+        public byte[] getPayload()
+        {
+            return payload;
+        }
+    }
+    
+    public int getMessageCount()
+    {
+        synchronized (messages)
+        {
+            return messages.size();
+        }
+    }
+    
+    public void clear()
+    {
+        synchronized (messages)
+        {
+            messages.clear();
+        }
+    }
+    
+    public Message retrieve()
+    {
+        synchronized (messages)
+        {
+            if (messages.isEmpty()) return null;
+            return messages.remove(0);
+        }
+    }
+    
+    
+    protected final List<Message> messages=new ArrayList<Message>();
+    
+    protected Consumer consumer=new Consumer() {
+        
+        public void handleShutdownSignal(String consumerTag, ShutdownSignalException sig)
+        {
+        }
+        
+        public void handleRecoverOk(String consumerTag)
+        {
+        }
+        
+        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+        {
+            System.err.println(envelope.getRoutingKey());
+            synchronized (messages)
+            {
+                messages.add(new Message(envelope.getRoutingKey(),body));
+                messages.notify();
+            }
+        }
+        
+        public void handleConsumeOk(String consumerTag)
+        {
+        }
+        
+        public void handleCancelOk(String consumerTag)
+        {
+        }
+        
+        public void handleCancel(String consumerTag) throws IOException
+        {
+        }
+    };
+
+}
